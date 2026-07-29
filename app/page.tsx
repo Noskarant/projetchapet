9e94d1f88b57c8a52f60a1cd0cf6abd248010c22
Noskarant <tutomod@gmail.com>
2026-07-29 21:14:13 +0200

    Active le mode IA dans l’application

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import FunctionalPrototype from "./functional-prototype";
 import ProductEnhancements from "./product-enhancements";
+import AiChain from "./ai-chain";
 
 export default function Page() {
   return (
     <>
       <FunctionalPrototype />
       <ProductEnhancements />
+      <AiChain />
     </>
   );
 }
