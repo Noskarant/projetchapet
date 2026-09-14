9aa32ddb586e3d428ac1a711f65a8ff2cba0f6fc
Noskarant <tutomod@gmail.com>
2026-09-14 15:26:41 +0200

    Partager la configuration publique Supabase côté client et serveur

---
+++ lib/supabase-config.ts
@@ -0,0 +1,8 @@
+export const supabasePublicConfig = {
+  url:
+    process.env.NEXT_PUBLIC_SUPABASE_URL ??
+    "https://mdpmpuurdhdmeupqsmal.supabase.co",
+  publishableKey:
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ??
+    "sb_publishable_JRp1qHa87O5-bnHiWeZEqQ_4EANKzkN",
+};
